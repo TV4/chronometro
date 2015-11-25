2240ab20bec4096a5f314f57673fc39eee2edf62
DimitrisLachanas <dimitris.lachanas@apegroup.com>
2015-11-25 12:25:29 +0100

    use async task for simulating operations

--- chronometro-example/src/main/java/se/tv4/chronometro/MainActivity.java
+++ chronometro-example/src/main/java/se/tv4/chronometro/MainActivity.java
@@ -1,6 +1,9 @@
 package se.tv4.chronometro;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,7 +11,7 @@ import android.widget.TextView;
 
 import se.tv4.chronometro.annotation.LogUILoadingTime;
 
-
+@TargetApi(Build.VERSION_CODES.CUPCAKE)
 public class MainActivity extends Activity {
 
     private TextView mainTextview;
@@ -18,49 +21,81 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainTextview = (TextView)findViewById(R.id.main_textview);
+        mainTextview = (TextView) findViewById(R.id.main_textview);
         mainTextview.setText("Start");
         startDoing();
     }
 
-    @LogUILoadingTime(state = LogUILoadingTime.START, name = "MainActivity")
     private void startDoing() {
         startDo1();
     }
 
     @LogUILoadingTime(state = LogUILoadingTime.END, name = "MainActivity")
     private void endDo() {
-//        try {
-//
-//            Thread.sleep(1000, 0);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        mainTextview.setText("Finished!");
+        new AsyncTask<Object, Object, Void>() {
+            @Override
+            protected Void doInBackground(Object[] params) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                mainTextview.setText("Finished!");
+            }
+        }.execute();
+
     }
 
     @LogUILoadingTime(state = LogUILoadingTime.CHECKPOINT_END, name = "MainActivity")
     private void startDo2() {
-//        try {
-//
-//            Thread.sleep(2000, 0);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        mainTextview.setText("Loading more...");
-        endDo();
+        new AsyncTask<Object, Object, Void>() {
+            @Override
+            protected Void doInBackground(Object[] params) {
+                try {
+                    Thread.sleep(1000);
+                    endDo();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                mainTextview.setText("Loading more...");
+            }
+        }.execute();
     }
+
 
     @LogUILoadingTime(state = LogUILoadingTime.CHECKPOINT_START, name = "MainActivity")
     private void startDo1() {
-//        try {
-//
-//            Thread.sleep(1500, 0);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        mainTextview.setText("Loading...");
-        startDo2();
+        new AsyncTask<Object, Object, Void>() {
+            @Override
+            protected Void doInBackground(Object[] params) {
+                try {
+                    Thread.sleep(1000);
+                    startDo2();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                mainTextview.setText("Loading...");
+            }
+        }.execute();
+
     }
 
     @Override
